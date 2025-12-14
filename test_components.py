679a28e67c2cf203c8e97a77458e4e56781405e6
Filip Rzepkowski <filip.rzepkowski.stud@pw.edu.pl>
2025-12-14 20:00:42 +0100

    fixed typos in tests

--- test_components.py
+++ test_components.py
@@ -9,7 +9,7 @@ import pytest
 
 # Tests for Rotor
 def test_rotor_create_():
-    rotor = Rotor(notch_position='A', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', initial_poition='B', ring_setting='B')
+    rotor = Rotor(notch_position='A', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', initial_position='B', ring_setting='B')
     assert rotor.notch_position == 0
     assert rotor.initial_position == 1
     assert rotor.wiring == (
@@ -19,7 +19,7 @@ def test_rotor_create_():
 
 
 def test_rotor_create_lower_notch_postion():
-    rotor = Rotor(notch_position='a', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', initial_poition='B', ring_setting='c')
+    rotor = Rotor(notch_position='a', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', initial_position='B', ring_setting='c')
     assert rotor.notch_position == 0
     assert rotor.initial_position == 1
     assert rotor.wiring == (
@@ -29,7 +29,7 @@ def test_rotor_create_lower_notch_postion():
 
 
 def test_rotor_create_ring_setting_default():
-    rotor = Rotor(notch_position='a', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', initial_poition='B')
+    rotor = Rotor(notch_position='a', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCJ', initial_position='B')
     assert rotor.notch_position == 0
     assert rotor.initial_position == 1
     assert rotor.wiring == (
@@ -39,7 +39,7 @@ def test_rotor_create_ring_setting_default():
 
 
 def test_rotor_create_wiring_lower():
-    rotor = Rotor(notch_position='a', wiring='ekmflgdqvzntowyhxuspaibrcj', initial_poition='B')
+    rotor = Rotor(notch_position='a', wiring='ekmflgdqvzntowyhxuspaibrcj', initial_position='B')
     assert rotor.notch_position == 0
     assert rotor.initial_position == 1
     assert rotor.wiring == (
@@ -49,57 +49,57 @@ def test_rotor_create_wiring_lower():
 
 def test_rotor_create_notch_position_int():
     with pytest.raises(RotorConfigurationNotchPositionError):
-        Rotor(notch_position=1, initial_poition='c', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+        Rotor(notch_position=1, initial_position='c', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
 
 
 def test_rotor_create_notch_position_more_than_1():
     with pytest.raises(RotorConfigurationNotchPositionError):
-        Rotor(notch_position='ab', initial_poition='c', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+        Rotor(notch_position='ab', initial_position='c', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
 
 
 def test_rotor_create_notch_position_is_not_alpha():
     with pytest.raises(RotorConfigurationNotchPositionError):
-        Rotor(notch_position='!', initial_poition='c', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+        Rotor(notch_position='!', initial_position='c', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
 
 
 def test_rotor_create_wiring_long_len():
     with pytest.raises(RotorConfigurationWiringError):
-        Rotor(notch_position='a', wiring="EKMFLWYHXUSPAIBRCJ", initial_poition='c')
+        Rotor(notch_position='a', wiring="EKMFLWYHXUSPAIBRCJ", initial_position='c')
 
 
 def test_rotor_create_wiring_not_alpha():
     with pytest.raises(RotorConfigurationWiringError):
-        Rotor(notch_position='a', wiring="EKMFLGDQVZNTO2YHXUSPAIBRC!", initial_poition='c')
+        Rotor(notch_position='a', wiring="EKMFLGDQVZNTO2YHXUSPAIBRC!", initial_position='c')
 
 
 def test_rotor_create_wiring_is_number():
     with pytest.raises(RotorConfigurationWiringError):
-        Rotor(notch_position='a', wiring=84710293847561029384756102, initial_poition='c')
+        Rotor(notch_position='a', wiring=84710293847561029384756102, initial_position='c')
 
 
 def test_rotor_create_wiring_is_str_of_nums():
     with pytest.raises(RotorConfigurationWiringError):
-        Rotor(notch_position='a', wiring="84710293847561029384756102", initial_poition='c')
+        Rotor(notch_position='a', wiring="84710293847561029384756102", initial_position='c')
 
 
 def test_rotor_create_initial_position_not_alpha():
     with pytest.raises(RotorConfigurationInitialPositionError):
-        Rotor(notch_position='a', initial_poition='!', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+        Rotor(notch_position='a', initial_position='!', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
 
 
 def test_rotor_create_initial_position_wrong_length():
     with pytest.raises(RotorConfigurationInitialPositionError):
-        Rotor(notch_position='a', initial_poition='bc', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+        Rotor(notch_position='a', initial_position='bc', wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
 
 
 def test_rotor_create_initial_position_not_str():
     with pytest.raises(RotorConfigurationInitialPositionError):
-        Rotor(notch_position='a', initial_poition=1, wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
+        Rotor(notch_position='a', initial_position=1, wiring="EKMFLGDQVZNTOWYHXUSPAIBRCJ")
 
 
 def test_rotor_create_non_ascii_characters():
     with pytest.raises(RotorConfigurationWiringError):
-        Rotor(notch_position='A', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCŁ', initial_poition='B', ring_setting='B')
+        Rotor(notch_position='A', wiring='EKMFLGDQVZNTOWYHXUSPAIBRCŁ', initial_position='B', ring_setting='B')
 
 
 # Tests for Reflector
