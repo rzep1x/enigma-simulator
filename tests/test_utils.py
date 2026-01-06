aa447564fef23a4cb065e4693f3230061711b392
Filip <01206229@pw.edu.pl>
2026-01-06 17:38:23 +0100

    added some tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,3 +9,11 @@ def test_char_to_int():
 def test_int_to_char():
     assert int_to_char(0) == 'A'
     assert int_to_char(2) == 'C'
+
+
+def test_int_to_char_negative_num():
+    assert int_to_char(-1) == 'Z'
+
+
+def test_int_to_char_number_greater_than_25():
+    assert int_to_char(26) == "A"
